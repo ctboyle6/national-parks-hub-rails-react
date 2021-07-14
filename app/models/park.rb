b90e8ba9314155ede3a4cc5bc2ef515fdf4bf361
Chris Boyle <cthboyle6@gmail.com>
2021-07-14 19:17:37 -0400

    park model validations and avg rating calc

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,11 @@
 class Park < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+
+  def avg_rating
+    return 0 unless reviews.count.positive?
+
+    reviews.average(:rating).round(2).to_f
+  end
 end
