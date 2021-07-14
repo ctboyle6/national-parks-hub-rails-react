c83261abb05a3e5702a148e2d7892d07e2f4725e
Chris Boyle <cthboyle6@gmail.com>
2021-07-14 19:43:07 -0400

    added seed file, test seeds, and geocoder gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+# tear down
+puts "Clearing parks.."
+
+Park.destroy_all
+
+puts "Parks cleared."
+
+
+#run db:seed from main project folder
+filepath    = './db/parks.csv'
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+
+puts "Loading parks.."
+
+CSV.foreach(filepath, csv_options) do |row|
+  # Park.new(park_code: row[0], name: row[1])
+  puts "#{row[0]} - name: #{row[1]}"
+  if Geocoder.search("#{row[1]}").first.nil?
+    next
+  else
+    coords = Geocoder.search("#{row[1]}").first.coordinates
+    # puts "lat: #{coords[0]}"
+    # puts "lon: #{coords[1]}"
+    Park.create(park_code: row[0], name: row[1], latitude: coords[0], longitude: coords[1])
+    puts "#{row[1]} created"
+  end
+end
+
+# puts Geocoder.search("Yellowstone National Park").first.coordinates
+
+puts "Parks loaded."
+
+# puts "Adding a few reviews.."
+
+# reviews = Review.create([
+#   {
+#     title: "Great trip",
+#     description: "This place had everything to offer.",
+#     rating: 5,
+#     park: Park.first
+#   },
+#   {
+#     title: "Worst park",
+#     description: "This is hell",
+#     rating: 1,
+#     park: Park.first
+#   },
+
+# ])
