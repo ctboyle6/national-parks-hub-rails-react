9b5332173903b20740bf5cdd89fcf7a2333b5a5e
Chris Boyle <cthboyle6@gmail.com>
2021-07-14 19:24:07 -0400

    db migration for park model

--- db/migrate/20210714231454_create_parks.rb
+++ db/migrate/20210714231454_create_parks.rb
@@ -1,11 +1,11 @@
 class CreateParks < ActiveRecord::Migration[6.0]
   def change
     create_table :parks do |t|
-      t.string :name
-      t.float :latitude
-      t.float :longitude
-      t.string :park_code
-      t.string :image
+      t.string :name, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
+      t.string :park_code, null: false
+      t.string :image, default: 'https://www.logolynx.com/topic/national+park+service#&gid=1&pid=1'
 
       t.timestamps
     end
