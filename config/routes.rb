cbf036e5dfe2f2c5546b64e0ab80e85a59d5890f
Chris Boyle <cthboyle6@gmail.com>
2021-07-14 19:32:22 -0400

    routes for park index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'parks/index'
+      get 'parks/:id', to: 'parks#show'
+    end
+  end
   root 'home#index'
+  get '/*path' => 'home#index'
 end
