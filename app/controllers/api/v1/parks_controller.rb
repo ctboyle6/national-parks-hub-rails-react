fea8c535890635690097edaa743a64335754188e
Chris Boyle <cthboyle6@gmail.com>
2021-07-14 19:39:29 -0400

    park api controller actions

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::ParksController < ApplicationController
   def index
+    parks = Park.all.order(name: :asc)
+    render json: parks
   end
 
   def show
+    if park
+      render json: park
+    else
+      render json: park.errors
+    end
+  end
+
+  private
+
+  def park
+    @park ||= Park.find(params[:id])
   end
 end
