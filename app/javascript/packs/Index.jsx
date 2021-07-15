03e655ae8b50a98b73bcc659ef5b50f650503110
Chris Boyle <cthboyle6@gmail.com>
2021-07-14 20:52:27 -0400

    moved redux to index entry file, out of index routes file

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,14 +1,43 @@
+// React
 import React from "react";
 import { render } from "react-dom";
+
+// Style
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+// Redux
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import userReducer from '../reducers/user_reducer'
+import parksReducer  from '../reducers/parks_reducer'
+import parkReducer from '../reducers/park_reducer'
+import reviewsReducer from '../reducers/reviews_reducer'
+import { logger } from 'redux-logger'
+import reduxPromise from 'redux-promise'
+import thunk from 'redux-thunk'
+
+// Components
 import App from "../components/App";
+
+const reducers = combineReducers({
+  user: userReducer,
+  parks: parksReducer,
+  park: parkReducer,
+  reviews: reviewsReducer
+})
+
+const middlewares = applyMiddleware(logger, reduxPromise, thunk);
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
-    <App />,
+    <Provider store={createStore(reducers, {}, middlewares)}>
+      <Router>
+        <Route path="/" component={App}/>
+      </Router>
+    </Provider>,
     document.body.appendChild(document.createElement("div"))
   );
 });
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,29 +2,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-// Redux
-import { Provider } from 'react-redux'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import userReducer from '../reducers/user_reducer'
-import parksReducer  from '../reducers/parks_reducer'
-import parkReducer from '../reducers/park_reducer'
-import reviewsReducer from '../reducers/reviews_reducer'
-import { logger } from 'redux-logger'
-import reduxPromise from 'redux-promise'
-import thunk from 'redux-thunk'
-
 // Components
 import Home from "../components/Home";
 import Parks from "../components/Parks";
-
-const reducers = combineReducers({
-  user: userReducer,
-  parks: parksReducer,
-  park: parkReducer,
-  reviews: reviewsReducer
-})
-
-const middlewares = applyMiddleware(logger, reduxPromise, thunk);
 
 export default (
   <Router>
